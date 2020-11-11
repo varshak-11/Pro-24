715c4b5a2352882efb9aedc9445c42b56d5c2140
varshak-11 <72525719+varshak-11@users.noreply.github.com>
2020-11-11 17:03:13 -0500

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class paper{
+    constructor(x, y,diameter) {
+        var options = {
+            'isStatic':false,
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.2,
+        }
+        this.x=x
+        this.y=y
+        this.diameter=diameter
+        
+        this.body = Bodies.circle(this.x,this.y,this.diameter/2,options);
+        World.add(world, this.body);
+      }
+      
+      display(){
+        var paperpos=this.body.position
+        var angle = this.body.angle;
+        push();
+        translate(paperpos.x, paperpos.y);
+        rectMode(CENTER);
+        strokeWeight(3);
+        fill(255,0,255);
+        ellipse(0,0,this.diameter,this.diameter)
+        pop();
+      }
+}
